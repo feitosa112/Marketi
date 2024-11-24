ca7a2a46b4c47e4e78360d1b540c4b433f0f7126
feitosa112 <112kuzmanovic@gmail.com>
2024-11-24 12:29:37 +0100

    create grad table

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Builder::defaultStringLength(191);
+         Schema::defaultStringLength(191);
+
     }
 }
